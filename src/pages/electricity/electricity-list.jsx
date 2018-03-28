2b9d4d7ed9b9fa5eae8dfcbd176247162d94afdb
Pavel Iarov <pav.yarov@gmail.com>
2018-03-29 01:45:14 +0300

    responsive electricity-list

--- src/pages/electricity/electricity-list.jsx
+++ src/pages/electricity/electricity-list.jsx
@@ -1,18 +1,29 @@
 import * as React from 'react';
-import { List, Datagrid, TextField, NumberField, ShowButton } from 'admin-on-rest';
+import { List, Responsive, SimpleList, Datagrid, TextField, NumberField, ShowButton } from 'admin-on-rest';
 
 export const ElectricityList = (props) => (
     <List title="Электроэнергия" {...props}>
-        <Datagrid>
-            <TextField label="Период" source="period" />
-            <TextField label="Владелец" source="fio" />
-            <TextField label="Адрес" source="address" />
-            <NumberField label="Начало, kWh" source="startKwh" />
-            <NumberField label="Расход, kWh" source="expenseKwh" />
-            <NumberField label="Конец, kWh" source="finishKwh" />
-            <NumberField label="Цена за kWh" source="price" />
-            <NumberField label="Сумма к оплате" source="checkoutPrice" />
-            <ShowButton />
-        </Datagrid>
+        <Responsive
+            small={
+                <Datagrid>
+                    <TextField label="Период" source="period" />
+                    <NumberField label="Сумма к оплате" source="checkoutPrice" />
+                    <ShowButton />
+                </Datagrid>
+            }
+            medium={
+                <Datagrid>
+                    <TextField label="Период" source="period" />
+                    <TextField label="Владелец" source="fio" />
+                    <TextField label="Адрес" source="address" />
+                    <NumberField label="Начало, kWh" source="startKwh" />
+                    <NumberField label="Расход, kWh" source="expenseKwh" />
+                    <NumberField label="Конец, kWh" source="finishKwh" />
+                    <NumberField label="Цена за kWh" source="price" />
+                    <NumberField label="Сумма к оплате" source="checkoutPrice" />
+                    <ShowButton />
+                </Datagrid>
+            }
+        />
     </List>
 );
